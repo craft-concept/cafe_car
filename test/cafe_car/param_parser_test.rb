113f8a8b6ce6bcebabdd2270e43f6574677dd153
Jeff Peterson <jeff@yak.sh>
2025-07-08 12:46:36 -0400

    Handle invalid ranges in ParamParser

--- test/cafe_car/param_parser_test.rb
+++ test/cafe_car/param_parser_test.rb
@@ -1,9 +1,16 @@
-require 'minitest/autorun'
+require "test_helper"
 
 module CafeCar
-  class ParamParserTest < Minitest::Test
-    def test
-      skip 'Not implemented'
+  class ParamParserTest < ActiveSupport::TestCase
+    test "handles invalid range string" do
+      parser = ParamParser.new({"a" => "1..2..3"})
+      assert_equal "1..2..3", parser.parsed[:a]
+    end
+
+    test "parses valid ranges" do
+      parser = ParamParser.new({"a" => "1..2", "b" => "3...5"})
+      assert_equal "1".."2", parser.parsed[:a]
+      assert_equal "3"..."5", parser.parsed[:b]
     end
   end
 end
