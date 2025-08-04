4b47940fcefb7953f130e1ccad4696ae6ccda2c8
Jeff Peterson <jeff@yak.sh>
2025-08-04 16:39:42 -0400

    generate cafe_car:resource should generate model

--- lib/generators/cafe_car/resource/resource_generator.rb
+++ lib/generators/cafe_car/resource/resource_generator.rb
@@ -4,6 +4,11 @@ class CafeCar::ResourceGenerator < Rails::Generators::NamedBase
   include Rails::Generators::ResourceHelpers
 
   source_root File.expand_path("templates", __dir__)
+  argument :attributes, type: :array, default: [], banner: "field[:type][:index] field[:type][:index]"
+
+  def create_model
+    generate "model", file_path, *attributes, force, inline: true
+  end
 
   def create_controller
     generate "cafe_car:controller", controller_name, force, inline: true
