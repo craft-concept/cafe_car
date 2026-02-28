84b92d9dc8650ab42fee874a5b257ae51b577078
Jeff Peterson <jeff@yak.sh>
2026-02-28 17:27:07 -0500

    fix turbo_stream links not rendering html by default

--- lib/cafe_car/application_responder.rb
+++ lib/cafe_car/application_responder.rb
@@ -7,5 +7,11 @@ module CafeCar
 
     self.error_status    = :unprocessable_entity
     self.redirect_status = :see_other
+
+    def to_turbo_stream
+      # Put :html back in the accepted format list. respond_with removes it
+      controller.lookup_context.formats << :html
+      to_html
+    end
   end
 end
