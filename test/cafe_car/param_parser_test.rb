c72367a3d3a5d9bc088dca0edc6f5a442a5d6e92
jeffpeterson <1184256+jeffpeterson@users.noreply.github.com>
2026-03-01 03:21:49 +0000

    [create-pull-request] automated change

--- test/cafe_car/param_parser_test.rb
+++ test/cafe_car/param_parser_test.rb
@@ -4,11 +4,13 @@ module CafeCar
   class ParamParserTest < ActiveSupport::TestCase
     test "handles invalid range string" do
       parser = ParamParser.new({"a" => "1..2..3"})
+
       assert_equal "1..2..3", parser.parsed[:a]
     end
 
     test "parses valid ranges" do
       parser = ParamParser.new({"a" => "1..2", "b" => "3...5"})
+
       assert_equal "1".."2", parser.parsed[:a]
       assert_equal "3"..."5", parser.parsed[:b]
     end
